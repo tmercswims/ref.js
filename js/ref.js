6f41b19ce566cd1d905dea7c47e9721e3a28b497
Thomas Mercurio <thomas_mercurio@brown.edu>
2015-02-16 10:34:03 -0500

    Added script and style.

---
+++ js/ref.js
@@ -0,0 +1,20 @@
+function ref_init() {
+  jQuery(".ref").each(function(index) {
+    jQuery(this).html('<a id="ref' + (index+1) + '"><span class="refnum" id="ref' + (index+1) + '">[' + (index+1) + ']</span><span class="refbody" style="display: none;">' + jQuery(this).html() + "</span></a>")
+  });
+
+  jQuery(function() {
+    jQuery(".refbody").hide();
+
+    jQuery(".refnum").click(function(event) {
+      jQuery(this.nextSibling).toggle();
+      event.stopPropagation();
+    });
+
+    jQuery("body").click(function() {
+      jQuery(".refbody").hide();
+    });
+  });
+}
+
+jQuery(document).ready(ref_init);
