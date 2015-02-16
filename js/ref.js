5e2586e384d38ee2598728b12c4ffa43a6b277e5
Thomas Mercurio <thomas_mercurio@brown.edu>
2015-02-16 12:07:14 -0500

    Updated for clicked ones, and added minified files.

--- js/ref.js
+++ js/ref.js
@@ -1,12 +1,13 @@
 function ref_init() {
   jQuery(".ref").each(function(index) {
-    jQuery(this).html('<a id="ref' + (index+1) + '"><span class="refnum" id="ref' + (index+1) + '">[' + (index+1) + ']</span><span class="refbody" style="display: none;">' + jQuery(this).html() + "</span></a>")
+    jQuery(this).html('<a id="ref' + (index+1) + '"><span class="refnum" id="ref' + (index+1) + '" clicked="0">[' + (index+1) + ']</span><span class="refbody" style="display: none;">' + jQuery(this).html() + "</span></a>")
   });
 
   jQuery(function() {
     jQuery(".refbody").hide();
 
     jQuery(".refnum").click(function(event) {
+      jQuery(this).attr("clicked", 1);
       jQuery(this.nextSibling).toggle();
       event.stopPropagation();
     });
